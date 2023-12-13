5a20e549a4201ab885d4ab3346346bd9411e5284
AshinrajM <ashinrajeevank@outlook.com>
2023-12-13 20:45:45 +0530

    searches

--- Searches.py
+++ Searches.py
@@ -1,4 +1,4 @@
-# Binary is a search with O(log n) time complexity but the array or list has to be in sorted order
+# Binary Search is a search with O(log n) time complexity but the array has to be in sorted order
 
 def BinarySearch(arr,value):
     start=0
@@ -20,7 +20,36 @@ arr=[1,2,3,4,5,6,7,8,9]
 print(BinarySearch(arr,13))
 
 
-# Linear search 
+# Recursive Binary Search
+
+
+def RecursiveBinarySearch(arr, value, start=0, end=None):
+    
+    if end is None:
+        end=len(arr)-1
+    
+    if start<=end:
+        mid=(start+end)//2
+
+        if arr[mid]==value:
+            return mid
+
+        elif value < arr[mid]:                                                                   
+            end = mid-1
+            return RecursiveBinarySearch(arr,value,start,end)
+        else:
+            start=mid+1
+            return RecursiveBinarySearch(arr,value,start,end)
+    else:
+        return "Not Found"
+
+
+
+arr = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+print(RecursiveBinarySearch(arr, 11))
+
+
+Linear search
 
 def LinearSearch(value,arr):
     for i in range(len(arr)):
